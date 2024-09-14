353ec90e2c506241b9e0fc984e10e58045e40e34
Yogesh <longquest.oyscommunity@gmail.com>
2024-09-14 11:36:51 +0530

    LEET-01

---
+++ Two_Sum_01/Main.java
@@ -0,0 +1,12 @@
+package Two_Sum_01;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] a ={2,7,11,15};
+        int b = 9;
+        Solution obj = new Solution();
+        System.out.println(Arrays.toString(obj.twoSum(a,b)));
+    }
+}
